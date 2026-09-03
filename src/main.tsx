448b973194bb0952268aabfc9a7bfd24bf2bce02
Muhammad Ihsan Ramadhan <121941162+ihsan-ramadhan@users.noreply.github.com>
2026-09-03 10:53:47 +0800

    add custom context menu for input/textarea

--- src/main.tsx
+++ src/main.tsx
@@ -2,16 +2,14 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "sonner";
+import ContextMenuProvider from "./components/ContextMenuProvider";
 import App from "./App";
 import "./globals.css";
-
-document.addEventListener("contextmenu", (e) => {
-  e.preventDefault();
-});
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
+      <ContextMenuProvider />
       <Toaster richColors position="top-right" />
       <App />
     </BrowserRouter>
